d59202262bbab66f9cbe3838260c8b3cb8d7ecf1
Massimiliano Dessì - (Fast Chauffeur) <desmax74@users.noreply.github.com>
2025-04-11 16:49:46 +0200

    Fx for PSQL admin db (#61)
    * Fix image url (#60)
    
    Signed-off-by: desmax74 <mdessi@redhat.com>
    
    * Fix for PSQL admin db (#52)
    
    Signed-off-by: Rajan Ravi <rravi@rravi-thinkpadp1gen4i.bengluru.csb>
    Co-authored-by: Rajan Ravi <rravi@rravi-thinkpadp1gen4i.bengluru.csb>
    
    ---------
    
    Signed-off-by: desmax74 <mdessi@redhat.com>
    Signed-off-by: Rajan Ravi <rravi@rravi-thinkpadp1gen4i.bengluru.csb>
    Co-authored-by: Rajan Ravi <86094767+mrrajan@users.noreply.github.com>
    Co-authored-by: Rajan Ravi <rravi@rravi-thinkpadp1gen4i.bengluru.csb>

--- roles/tpa_single_node/templates/configs/create-importers.sql
+++ roles/tpa_single_node/templates/configs/create-importers.sql
@@ -1,5 +1,5 @@
 INSERT INTO IMPORTER (name, revision, state, last_change, configuration)
-VALUES ('cve', gen_random_uuid(), 0, now(), '{"cve":{"description":"CVE list v5","period":"1d","source":"https://github.com/CVEProject/cvelistV5"}}'::jsonb)
+VALUES ('cve', gen_random_uuid(), 0, now(), '{"cve":{"description":"CVE list v5","period":"1d","disabled": false,"source":"https://github.com/CVEProject/cvelistV5"}}'::jsonb)
 ON CONFLICT (name) DO UPDATE
 SET
   revision=EXCLUDED.revision,
@@ -7,7 +7,7 @@ SET
 ;
 
 INSERT INTO IMPORTER (name, revision, state, last_change, configuration)
-VALUES ('redhat-csaf', gen_random_uuid(), 0, now(), '{"csaf":{"description":"All Red Hat CSAF data","fetchRetries":50,"period":"1d","source":"redhat.com"}}'::jsonb)
+VALUES ('redhat-csaf', gen_random_uuid(), 0, now(), '{"csaf":{"description":"All Red Hat CSAF data","fetchRetries":50,"disabled": true,"period":"1d","source":"redhat.com"}}'::jsonb)
 ON CONFLICT (name) DO UPDATE
 SET
   revision=EXCLUDED.revision,
@@ -15,7 +15,7 @@ SET
 ;
 
 INSERT INTO IMPORTER (name, revision, state, last_change, configuration)
-VALUES ('redhat-sboms', gen_random_uuid(), 0, now(), '{"sbom":{"description":"All Red Hat SBOMs","fetchRetries":50,"keys":["https://access.redhat.com/security/data/97f5eac4.txt#77E79ABE93673533ED09EBE2DCE3823597F5EAC4"],"period":"1d","source":"https://access.redhat.com/security/data/sbom/beta/"}}'::jsonb)
+VALUES ('redhat-sboms', gen_random_uuid(), 0, now(), '{"sbom":{"description":"All Red Hat SBOMs","fetchRetries":50,"disabled": true,"keys":["https://access.redhat.com/security/data/97f5eac4.txt#77E79ABE93673533ED09EBE2DCE3823597F5EAC4"],"period":"1d","source":"https://access.redhat.com/security/data/sbom/beta/"}}'::jsonb)
 ON CONFLICT (name) DO UPDATE
 SET
   revision=EXCLUDED.revision,
@@ -23,7 +23,7 @@ SET
 ;
 
 INSERT INTO IMPORTER (name, revision, state, last_change, configuration)
-VALUES ('osv-github', gen_random_uuid(), 0, now(), '{"osv":{"description":"GitHub Advisory Database","period":"1d","source":"https://github.com/github/advisory-database"}}'::jsonb)
+VALUES ('osv-github', gen_random_uuid(), 0, now(), '{"osv":{"description":"GitHub Advisory Database","period":"1d","disabled": false,"source":"https://github.com/github/advisory-database"}}'::jsonb)
 ON CONFLICT (name) DO UPDATE
 SET
   revision=EXCLUDED.revision,
